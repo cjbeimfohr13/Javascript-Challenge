933b1de76d4f138ceeec7c7a392d6e5a04457748
cjbeimfohr13 <68441397+cjbeimfohr13@users.noreply.github.com>
2020-11-08 09:31:39 -0500

    app.js commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -21,16 +21,30 @@ tableData.forEach(function(ufoSighting){
 
 var button = d3.select("#filter-btn"); 
 button.on("click",function() {
+    
+    d3.select("tbody").html("");
+    d3.event.preventDefault();
 
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
 
     console.log(inputValue);
-});
-    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
 
     console.log(filteredData);
 
-    // filteredData.forEach(function())
+    filteredData.forEach(function(sighting){
+    
+        console.log(sighting);
 
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(function([key,value]){
+        console.log(key,value);
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
 
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,49 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(tableData);
+
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(ufoSighting){
+    console.log(ufoSighting);
+
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key,value]){
+        console.log(key,value);
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+
+
+});
+
+var button = d3.select("#filter-btn"); 
+button.on("click",function() {
+    
+    d3.select("tbody").html("");
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    console.log(filteredData);
+
+    filteredData.forEach(function(sighting){
+    
+        console.log(sighting);
+
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(function([key,value]){
+        console.log(key,value);
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+    });
+});
