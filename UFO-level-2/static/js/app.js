d22e1eee94b5a115d060d1e0f69354cbdad1328d
cjbeimfohr13 <68441397+cjbeimfohr13@users.noreply.github.com>
2020-11-08 19:06:22 -0500

    app and index commit

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,16 +1,13 @@
 // from data.js
 var tableData = data;
 
-console.log(tableData);
 
 var tbody = d3.select("tbody");
 
 tableData.forEach(function(ufoSighting){
-    console.log(ufoSighting);
 
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(function([key,value]){
-        console.log(key,value);
 
         var cell = row.append("td");
         cell.text(value);
@@ -25,22 +22,46 @@ button.on("click",function() {
     d3.select("tbody").html("");
     d3.event.preventDefault();
 
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
+    var dateInputElement = d3.select("#datetime");
+    var dateInputValue = dateInputElement.property("value");
+    var cityInputElement = d3.select("#city");
+    var cityInputValue= cityInputElement.property("value");
+    var stateInputElement = d3.select("#state");
+    var stateInputValue= stateInputElement.property("value");
+    var countryInputElement = d3.select("#country");
+    var countryInputValue= countryInputElement.property("value");
+    var shapeInputElement = d3.select("#shape");
+    var shapeInputValue= shapeInputElement.property("value");
 
-    console.log(inputValue);
 
-    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+    var filteredData = tableData
+    if(dateInputValue!==""){
+        console.log('filtering by date: ', dateInputValue)
+        filteredData = tableData.filter(ufo => ufo.datetime === dateInputValue);
+    }
+    if(cityInputValue!=="") {
+        console.log('filtering by city: ', cityInputValue)
+        filteredData = filteredData.filter(ufo => ufo.city === cityInputValue);
+    }
+    if(stateInputValue!=="") {
+        console.log('filtering by state: ', stateInputValue)
+        filteredData = filteredData.filter(ufo => ufo.state === stateInputValue);
+    }
+    if(countryInputValue!=="") {
+        console.log('filtering by country: ', countryInputValue)
+        filteredData = filteredData.filter(ufo => ufo.country === countryInputValue);
+    }
+    if(shapeInputValue!=="") {
+        console.log('filtering by shape: ', shapeInputValue)
+        filteredData = filteredData.filter(ufo => ufo.shape === shapeInputValue);
+    }
 
-    console.log(filteredData);
 
     filteredData.forEach(function(sighting){
     
-        console.log(sighting);
 
         var row = tbody.append("tr");
         Object.entries(sighting).forEach(function([key,value]){
-        console.log(key,value);
 
         var cell = row.append("td");
         cell.text(value);
